25da1c5e388cc99bb16866792b08e58f28b2a1ad
Amr AboElhamd <amr.abuelhamd@gmail.com>
2020-04-28 15:10:12 +0700

    trying solid background fragment

--- app/src/main/java/com/arvatel/photoeditor/MainActivity.kt
+++ app/src/main/java/com/arvatel/photoeditor/MainActivity.kt
@@ -2,11 +2,19 @@ package com.arvatel.photoeditor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun startRotationFragment(view: View) {
+        val fragmentRotation = FragmentRotation.newInstance("ddd","ssss");
+        val fragmentActivity = supportFragmentManager
+        val fragmentTransitionImpl = fragmentActivity.beginTransaction()
+        fragmentTransitionImpl.add(R.id.fragment_container,fragmentRotation).addToBackStack(null).commit()
     }
 }
