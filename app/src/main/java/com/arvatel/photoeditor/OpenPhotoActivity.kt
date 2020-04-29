00982fe728cfc45c0c5020f17feceec6ed4df19c
Arvatel <namenori74@yandex.ru>
2020-04-29 23:45:14 +0700

    created activity for OpenPhoto

---
+++ app/src/main/java/com/arvatel/photoeditor/OpenPhotoActivity.kt
@@ -0,0 +1,11 @@
+package com.arvatel.photoeditor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OpenPhotoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
