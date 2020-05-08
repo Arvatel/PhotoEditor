986ced4aade5d975e159d2b70883e4180654189d
Arvatel <namenori74@yandex.ru>
2020-05-08 14:36:21 +0700

    added new fragments

---
+++ app/src/main/java/com/arvatel/photoeditor/PhotoEditorActivity.kt
@@ -0,0 +1,12 @@
+package com.arvatel.photoeditor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotoEditorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_editor)
+    }
+}
